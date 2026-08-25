6f32944aead65c10a7ce75d52f970895f254d1b7
Chris Nelson <chris.nelson@sap.com>
2026-08-25 14:07:03 -0400

    Created basic two variable function for add, subtract, multiply, and divide. Confirmed they worked via console.

---
+++ script.js
@@ -0,0 +1,24 @@
+// Create add function (two inputs)
+function add(a, b) {
+    return a + b;
+}
+
+// Create subtract function (two inputs)
+function subtract(a, b) {
+    return a - b;
+}
+
+// Create multiple function (two inputs)
+function multiply(a, b) {
+    return a * b;
+}
+
+// Create divide function (two inputs)
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(10, 20));
+console.log(subtract(10, 20));
+console.log(multiply(10, 20));
+console.log(divide(10, 20));
